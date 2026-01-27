cd09afc96bdfc4fcf4ebc6de8f75ceb21531ff33
İlter Fehmi Ataulusoy <MSekmet@gmail.com>
2026-01-27 16:43:05 +0300

    Animations (not used)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
     private void Update()
     {
         
@@ -28,8 +29,15 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime ;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 11f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
         
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
